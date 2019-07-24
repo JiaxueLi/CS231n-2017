4c6a3b9741ea117df759bb08b0b09b97eb022cad
JiaxueLi <49477420+JiaxueLi@users.noreply.github.com>
2019-07-24 21:56:04 +0800

    Update multiplygate.py

--- example/multiplygate.py
+++ example/multiplygate.py
@@ -1,9 +1,11 @@
 class MultiplyGate(object):
   def forward(x, y):
     z = x * y
+    self.x = x
+    self.y = y
     return z
   
   def backward(dz):
-    # dx = ... # todo
-    # dy = ... # todo
+    dx = self.y * dz #[dz/dx * dL/dz]
+    dy = self.x * dz #[dz/dy * dL/dz]
     return [dx, dy]
